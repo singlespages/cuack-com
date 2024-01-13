03ce56f80dd9180a0aac2e8a99567b86c472d587
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-01-13 14:30:52 -0500

    section about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   },
   {
     path: '',
