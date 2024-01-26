3c36e355f394423f6ec6d36f18f0c84d5d6b6f2d
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-01-26 18:59:59 -0500

    components contact and footer uniques

--- src/app/pages/custom-page-design/custom-page-design.component.ts
+++ src/app/pages/custom-page-design/custom-page-design.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ContactComponent} from "../contact/contact.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-custom-page-design',
   standalone: true,
-  imports: [],
+  imports: [
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './custom-page-design.component.html',
   styleUrl: './custom-page-design.component.scss'
 })
