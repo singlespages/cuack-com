38eeb5e926ec2efe1e72903bb7d2875b405ed2de
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-03-20 22:22:04 -0500

    blog section

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
 })
