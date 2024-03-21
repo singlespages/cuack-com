09e34643c8889961c382c4e04a4a7deda48c6ff8
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-03-20 23:12:48 -0500

    blog section

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-blog',
   standalone: true,
     imports: [
-        RouterLink
+        RouterLink,
+        FooterComponent
     ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
