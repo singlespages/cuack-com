9d3acef6e321ff9c8f965fefae0ce031fcf11635
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-03-21 10:24:38 -0500

    contact-us content

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {ContactComponent} from "../contact/contact.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
   imports: [
     RouterLink,
-    ContactComponent
+    ContactComponent,
+    FooterComponent
   ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
