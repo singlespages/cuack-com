450013b8b01d45c6ee5da87ab911d80e784fa04c
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-04-10 18:39:24 -0500

    home slider prev

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {SlickCarouselModule} from "ngx-slick-carousel";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterLinkActive,
-    RouterLink
+    RouterLink,
+    SlickCarouselModule
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
   isMenuOpen: boolean = false;
+  slideConfig = {
+    infinite: true,
+    "slidesToShow": 1,
+    dots: true,
+    arrows: false
+  };
+  slideConfigTwo = {
+    infinite: true,
+    "slidesToShow": 2,
+  };
 
   constructor(private router: Router) {}
 
@@ -28,6 +40,26 @@ export class HomeComponent {
   toggleMenu(event: Event): void {
     event.preventDefault();
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+
+  beforeChange(e: any) {
+    console.log('beforeChange');
   }
 
 }
