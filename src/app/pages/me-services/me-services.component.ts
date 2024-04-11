807b3dfb129264f42791323596147ece27a017b1
Mijail Gonzalez <mrgonzalez@gbm.net>
2024-04-10 21:19:30 -0500

    me services

--- src/app/pages/me-services/me-services.component.ts
+++ src/app/pages/me-services/me-services.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {SlickCarouselModule} from "ngx-slick-carousel";
 
 @Component({
   selector: 'app-me-services',
   standalone: true,
     imports: [
         RouterLink,
-        RouterLinkActive
+        RouterLinkActive,
+        SlickCarouselModule
     ],
   templateUrl: './me-services.component.html',
   styleUrl: './me-services.component.scss'
 })
 export class MeServicesComponent {
   isMenuOpen: boolean = false;
+  slideConfigTwo = {
+    infinite: true,
+    "slidesToShow": 1,
+    dots: true,
+    arrows: false
+  };
 
   constructor(private router: Router) {}
   scrollToTop() {
